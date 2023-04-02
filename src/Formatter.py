21bb9b9ae57fa792111d8418b1b4a7331a946323
WorkNicolas <workcarlnicolasvmendoza@gmail.com>
2023-04-02 18:39:01 +0800

    first commit

---
+++ src/Formatter.py
@@ -0,0 +1,107 @@
+"""
+Personal file renamer for the following format (ISO-8601):
+- yy-mm-ddThhmmH--[NAME]_File_Name.ext
+- yy-mm-ddThhmmH--File_Name.ext
+
+Rename to:
+- File-Name_[NAME]_yy-mm-ddThhmmH.ext
+
+Renames any files not under the exadox format to become the latter.
+
+Based on:
+- "https://exadox.com/en/articles/file-naming-convention-ten-rules-best-practice"
+"""
+
+__author__ = "Carl Nicolas Mendoza"
+__credits__ = ["Carl Nicolas Mendoza"]
+__email__ = "workcarlnicolasvmendoza@gmail.com"
+__version__ = "2023-04-02T1305H"
+
+import os
+import re
+from datetime import datetime
+
+# regex patterns
+date_pattern = r'\d{4}-\d{2}-\d{2}T\d{2}\d{2}H'
+bracket_pattern = r'\[.*?\]'
+hyphen_pattern = r'^(-[^-]*)?'
+
+# only file extensions in this list will be renamed
+exts = ['.docx', '.doc', '.odt', '.pptx', '.odp', '.png', '.jpg', '.jfif', '.jpeg', '.gif', '.bmp', '.pdf', '.xlsx', 'xls', '.txt']
+
+# determines if the previous conditional statements were executed
+executed = False
+queried = False
+
+# loop through all files in current directory
+file_size = [file for file in os.listdir('.')]
+
+# loops
+end = 0
+
+path = input("Enter Path: ")
+
+os.chdir(path)
+
+for i in range(0,2):
+    for filename in os.listdir(path):
+        # check if file is not a folder and contains two hyphens
+        # checking file extensions is not necessary since I only renamed files on exts
+        bracket = ''
+        if not os.path.isdir(filename) and '--' in filename:
+            # regex file extraction and file extension extraction
+            file_ext = os.path.splitext(filename)[1]
+            date_match = re.search(date_pattern, filename)
+            bracket_match = re.search(bracket_pattern, filename)
+            if date_match:
+                date = date_match.group()
+                try:
+                    bracket = bracket_match.group()
+                except AttributeError:
+                    bracket = ''
+                # extract original filename without date and bracket
+                orig_filename = re.sub(date_pattern + '|' + bracket_pattern + '|' + file_ext, '', filename).replace('--', '_')
+                # replace underscores with hyphens in filename
+                new_filename = orig_filename.replace('_', '-')
+                # insert date into new filename
+                new_filename = f'{new_filename}_{bracket}_{date}{file_ext}'
+                # removes '--' in filename
+                new_filename = new_filename.replace('--', '')
+                # rename file
+                if queried:
+                    os.rename(filename, new_filename)
+                print(f'{filename} to {new_filename}' if not queried else f'Renamed {filename} to {new_filename}')
+                executed = True
+        # checks if it is not a folder, contains an extension,
+        # and does not contain ISO-8601 date
+        elif not os.path.isdir(filename) and any(ext in filename for ext in exts) and not re.search(date_pattern, filename):
+                # generate ISO-8601 compliant date format
+                date_created = os.stat(filename).st_ctime
+                date_created_tstamp = datetime.fromtimestamp(date_created)
+                date = date_created_tstamp.strftime('%Y-%m-%dT%H%MH')
+                # extract file extension
+                file_ext = os.path.splitext(filename)[1]
+                # replace empty spaces with delimiter
+                new_filename = filename.replace(' ', '-')
+                # remove extension
+                for ext in exts:
+                    new_filename = new_filename.replace(ext, '')
+                # new file name
+                new_filename = f'{new_filename}_{date}{file_ext}'
+                # rename file
+                if queried:
+                    os.rename(filename, new_filename)
+                print(f'{filename} to {new_filename}' if not queried else f'Renamed {filename} to {new_filename}')
+                executed = True
+        elif not executed and end == len(file_size):
+            # reached the end of the loop without executing anything
+            print("No files were renamed")
+            i = 1
+        elif end == len(file_size) and not queried:
+            query = input("Accept Changes [Y/N]: ")
+            queried = True
+            if query.upper() == 'Y':
+                print("Changes accepted")
+            else:
+                print("Changes not accepted")
+        end += 1
